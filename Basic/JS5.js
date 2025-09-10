165fbc0a884700896d6731a833fa12736e56f9d6
Nayem Uddin <nayemuddin2006@gmail.com>
2025-09-10 22:14:21 +0600

    Add files via upload

---
+++ Basic/JS5.js
@@ -0,0 +1,16 @@
+var num = 20;
+
+num = toString(num);
+
+console.log(typeof(num));
+
+var num = "200";
+num = parseInt(num)
+
+console.log(typeof(num));
+
+ var number = 2.5775;
+ console.log(number.toFixed(2));
+
+ var number = 2.5685;
+ console.log(number.toPrecision(2));
